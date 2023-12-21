de61e11c38c4a60ea99429e8b528d0fda9f2e780
YX <sadyx019@gmail.com>
2023-12-21 09:43:37 +0800

    chore: Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { join } from 'path';
 
 export default defineConfig({
     root: join(__dirname, 'src'),
+    base: 'threejs-demo-collection',
     build: {
         outDir: join(__dirname, 'dist'),
         emptyOutDir: true,
