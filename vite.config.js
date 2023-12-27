1fa98411838b1986f9f22f94c736a2fb9208b5d7
yangxiong <978345079@qq.com>
2023-12-27 15:20:42 +0800

    chore: config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
-import { join } from 'path';
+import { join, resolve } from 'path';
 
 export default defineConfig({
     root: join(__dirname, 'src'),
+    base: 'threejs-demo-collection',
     build: {
         outDir: join(__dirname, 'dist'),
         emptyOutDir: true,
         sourcemap: true,
     },
+    resolve: {
+        alias:{
+            demos: resolve(__dirname, 'src/demos/'),
+        }
+    }
 });
