9fb93e4db46b176c0c4ef0fbed24fd59b50c8823
yangxiong <978345079@qq.com>
2023-12-29 09:44:17 +0800

    chore: update build config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,27 @@
 import { defineConfig } from 'vite';
-import { join, resolve } from 'path';
+import { join, relative, extname } from 'path';
+import { glob } from 'glob';
+import { fileURLToPath } from 'url';
 
 export default defineConfig({
     root: join(__dirname, 'src'),
     base: 'threejs-demo-collection',
     build: {
         outDir: join(__dirname, 'dist'),
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                index: 'src/index.html',
+                ...Object.fromEntries(
+                    glob.sync('src/demos/*.html').map(file => [
+                        relative(
+                            'src',
+                            file.slice(0, file.length - extname(file).length)
+                        ),
+                        fileURLToPath(new URL(file, import.meta.url)),
+                    ]),
+                ),
+            }
+        }
     },
 });
