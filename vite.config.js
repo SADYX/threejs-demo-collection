d6c1cdc674db1b3e2188ba7ed32148aea4761a00
sadyx <978345079@qq.com>
2024-04-11 00:49:12 +0800

    fix img url

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { fileURLToPath } from "url";
 export default defineConfig({
 	root: join(__dirname, "src"),
 	base: "/threejs-demo-collection/",
+	publicDir: '../public',
 	build: {
 		outDir: join(__dirname, "dist"),
 		emptyOutDir: true,
@@ -33,6 +34,5 @@ export default defineConfig({
 		alias: {
 			'@': resolve(__dirname, "src"),
 		}
-	},
-	assetsInclude: ["src/assets/screenshots/*"]
+	}
 });
