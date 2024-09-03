fe0e35f22f618b6083f65121e3eeab741f5a1af9
SADYX <zweihandler019@foxmail.com>
2024-09-03 15:29:01 +0800

    chore: format

--- vite.config.js
+++ vite.config.js
@@ -4,35 +4,35 @@ import { glob } from "glob";
 import { fileURLToPath } from "url";
 
 export default defineConfig({
-	root: join(__dirname, "src"),
-	base: "/threejs-demo-collection/",
-	publicDir: '../public',
-	build: {
-		outDir: join(__dirname, "dist"),
-		emptyOutDir: true,
-		rollupOptions: {
-			input: {
-				index: "src/index.html",
-				...Object.fromEntries(
-					glob
-						.sync("src/demos/*.html")
-						.map((file) => [
-							relative(
-								"src",
-								file.slice(
-									0,
-									file.length - extname(file).length
-								)
-							),
-							fileURLToPath(new URL(file, import.meta.url)),
-						])
-				),
-			},
-		},
-	},
-	resolve: {
-		alias: {
-			'@': resolve(__dirname, "src"),
-		}
-	}
+  root: join(__dirname, "src"),
+  base: "/threejs-demo-collection/",
+  publicDir: '../public',
+  build: {
+    outDir: join(__dirname, "dist"),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: "src/index.html",
+        ...Object.fromEntries(
+          glob
+            .sync("src/demos/*.html")
+            .map((file) => [
+              relative(
+                "src",
+                file.slice(
+                  0,
+                  file.length - extname(file).length
+                )
+              ),
+              fileURLToPath(new URL(file, import.meta.url)),
+            ])
+        ),
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, "src"),
+    }
+  }
 });
